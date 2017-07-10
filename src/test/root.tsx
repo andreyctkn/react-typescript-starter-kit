2c586b5855481982486afe886e312253d9328921
AndreyUtka <AndreyUtka@gmail.com>
2017-07-10 13:58:59 +0300

    fix scss build

--- src/test/root.tsx
+++ src/test/root.tsx
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 
 export class RootApp extends Component<any, any> {
   public render() {
-    console.warn("RootApp");
-    return <h1>Hello</h1>;
+    // console.warn("RootApp");
+    return <h1 className="sk_header">Hello122</h1>;
   }
 }
