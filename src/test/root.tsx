9b6faf23c4c6e824b070b414b2b4d46e056f1e55
AndreyUtka <AndreyUtka@gmail.com>
2017-09-06 15:47:19 +0300

    update typescript configuration

--- src/test/root.tsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-export class RootApp extends Component<any, any> {
-    public render() {
-        // console.warn("RootApp");
-        return <h1 className="sk_header">Hello122</h1>;
-    }
-}
