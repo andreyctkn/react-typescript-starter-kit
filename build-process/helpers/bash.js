7cd0d6ad2087b1f511ed7667152905c0b63fdcee
AndreyUtka <AndreyUtka@gmail.com>
2017-12-27 19:50:25 +0200

    add prettierignore

--- build-process/helpers/bash.js
+++ build-process/helpers/bash.js
@@ -1,25 +1,25 @@
-const chalk = require('chalk');
-const { promisify } = require('util');
-const { exec } = require('child_process');
+const chalk = require("chalk");
+const { promisify } = require("util");
+const { exec } = require("child_process");
 
 const promisedExec = promisify(exec);
 
 function logInfo(info) {
-	console.log(chalk`{cyan ${info}} \n`);
+    console.log(chalk`{cyan ${info}} \n`);
 }
 
 async function execCommand(...options) {
-	try {
-		const { stdout, stderr } = await promisedExec(options.join(' '));
-		if (stdout) {
-			console.log(`${stdout}`);
-		}
-		if (stderr) {
-			console.log(`${stderr}`);
-		}
-	} catch (error) {
-		console.log(chalk`{red ${error}}`);
-	}
+    try {
+        const { stdout, stderr } = await promisedExec(options.join(" "));
+        if (stdout) {
+            console.log(`${stdout}`);
+        }
+        if (stderr) {
+            console.log(`${stderr}`);
+        }
+    } catch (error) {
+        console.log(chalk`{red ${error}}`);
+    }
 }
 
 module.exports = { execCommand, logInfo };
--- build-process/make-vendor.js
+++ build-process/make-vendor.js
@@ -1,12 +1,12 @@
-const libsConfig = require('./libs');
-const buildConstants = require('./build-constants');
-const { execCommand, logInfo } = require('./helpers/bash');
+const libsConfig = require("./libs");
+const buildConstants = require("./build-constants");
+const { execCommand, logInfo } = require("./helpers/bash");
 const { BUILD_TYPE } = process.env;
 
-logInfo('concatenating libs to vendor.js');
+logInfo("concatenating libs to vendor.js");
 
 const vendorLibs = Object.keys(libsConfig)
-	.map(lib => BUILD_TYPE === 'production' ? libsConfig[lib].dev : libsConfig[lib].prod)
-	.join(' ');
+    .map(lib => (BUILD_TYPE === "production" ? libsConfig[lib].dev : libsConfig[lib].prod))
+    .join(" ");
 
-execCommand('cat', vendorLibs, '>', `${buildConstants.out}/vendor.js`);
+execCommand("cat", vendorLibs, ">", `${buildConstants.out}/vendor.js`);
