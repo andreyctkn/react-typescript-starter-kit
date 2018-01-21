02083290ced6df93d29e14ca53c0e5d4a211c8aa
AndreyUtka <AndreyUtka@gmail.com>
2018-01-21 02:11:50 +0200

    fix format

--- build-process/styles/watch.js
+++ build-process/styles/watch.js
@@ -1,4 +1,6 @@
 const { execSync } = require("child_process");
 const { INPUTS, OUTPUTS } = require("../constants");
 
-execSync(`onchange 'src/**/*.scss' -- node-sass ${INPUTS.scss} ${OUTPUTS.styles}`, { stdio: [0, 1, 2] });
+execSync(`onchange 'src/**/*.scss' -- node-sass ${INPUTS.scss} ${OUTPUTS.styles}`, {
+    stdio: [0, 1, 2],
+});
