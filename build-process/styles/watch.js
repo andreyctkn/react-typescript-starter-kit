24949dfc9851c983c9641a36fac8fb8c6af7fd79
AndreyUtka <AndreyUtka@gmail.com>
2018-01-21 02:05:08 +0200

    added csso

--- build-process/styles/watch.js
+++ build-process/styles/watch.js
@@ -1,4 +1,4 @@
 const { execSync } = require("child_process");
 const { INPUTS, OUTPUTS } = require("../constants");
 
-execSync(`node-sass ${INPUTS.scss} ${OUTPUTS.styles} --w`, { stdio: [0, 1, 2] });
+execSync(`onchange 'src/**/*.scss' -- node-sass ${INPUTS.scss} ${OUTPUTS.styles}`, { stdio: [0, 1, 2] });
